2a4d77978ddb0682623bac6bfcd2084c2591fee3
Ayushi Indu <singhayushi535@gmail.com>
2021-12-31 19:44:26 +0530

    Minor changes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let weather = {
-    apiKey: "API KEY HERE",
+    apiKey: "API_KEY_HERE",
     fetchWeather: function (city) {
       fetch(
         "https://api.openweathermap.org/data/2.5/weather?q=" +
